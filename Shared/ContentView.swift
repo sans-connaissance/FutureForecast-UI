845f3faf8fb9b341cda0bdfb23e9b1832d8cc423
sans-connaissance <davemalicke@gmail.com>
2021-07-11 13:29:43 -0400

    Added custom color and a number of UI Stacks

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -10,10 +10,42 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         ZStack {
-            LinearGradient(gradient: Gradient(colors: [.blue, .white]),
+            LinearGradient(gradient: Gradient(colors: [.blue, Color("lightBlue")]),
                            startPoint: .topLeading,
                            endPoint: .bottomTrailing)
                 .edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+            VStack {
+                Text("Ann Arbor, MI")
+                    .font(.system(size: 32, weight: .medium, design: .default))
+                    .foregroundColor(.white)
+                    .padding()
+                
+                VStack(spacing: 9){
+                    Image(systemName: "cloud.sun.fill")
+                        .renderingMode(.original)
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 180, height: 180)
+                    
+                    //hardcoded font
+                    Text("76°")
+                        .font(.system(size: 70, weight: .medium))
+                        .foregroundColor(.white)
+                }
+                .padding(.bottom, 40)
+                
+                HStack(spacing: 20) {
+                    WeatherDayView(dayOfWeek: "TUE", imageName: "cloud.sun.rain.fill", temp: 78)
+                    WeatherDayView(dayOfWeek: "WED", imageName: "cloud.bolt.fill", temp: 32)
+                    WeatherDayView(dayOfWeek: "THUR", imageName: "cloud.sleet.fill", temp: 50)
+                    WeatherDayView(dayOfWeek: "FRI", imageName: "sun.max.fill", temp: 65)
+                    WeatherDayView(dayOfWeek: "SAT", imageName: "cloud.sun.rain.fill", temp: 115)
+                    
+                    
+                }
+                Spacer()
+                
+            }
         }
     }
 }
@@ -21,5 +53,27 @@ struct ContentView: View {
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct WeatherDayView: View {
+    var dayOfWeek: String
+    var imageName: String
+    var temp: Int
+    
+    var body: some View {
+        VStack(spacing: 5) {
+            Text(dayOfWeek)
+                .font(.system(size: 16, weight: .medium, design: .default))
+                .foregroundColor(.white)
+            Image(systemName: imageName)
+                .renderingMode(.original)
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 40, height: 40)
+            Text("\(temp)°")
+                .font(.system(size: 28, weight: .medium, design: .default))
+                .foregroundColor(.white)
+        }
     }
 }
