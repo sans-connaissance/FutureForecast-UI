6f0da4188afe967264319412840255f289d664dc
sans-connaissance <davemalicke@gmail.com>
2021-07-11 11:42:29 -0400

    Initial Commit

---
+++ Shared/FutureForecast_UIApp.swift
@@ -0,0 +1,17 @@
+//
+//  FutureForecast_UIApp.swift
+//  Shared
+//
+//  Created by David Malicke on 7/11/21.
+//
+
+import SwiftUI
+
+@main
+struct FutureForecast_UIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
